709fc9876bf7dffef1985b4d90d18c1b9ed9ae21
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2023-09-04 02:41:19 +0800

    added fonts and root layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import './globals.css'
+import './static/globals.css'
 import type {Metadata} from "next";
 import localFont from 'next/font/local';
+import {Noto_Sans} from "next/font/google";
 
 const materialSymbols = localFont({
   variable: '--font-family-symbols',
   style: 'normal',
-  src: '../../node_modules/material-symbols/material-symbols-rounded.woff2',
+  src: './static/fonts/material-symbols-outlined.woff2',
   display: 'block',
   weight: '100 700',
 })
 
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  display: "auto",
+  style: ["normal", "italic"],
+  weight: ["400", "600", "700", "800"],
+  variable: "--font-noto-sans",
+})
 
+const sourceHansSans = localFont({
+  variable: '--font-source-hans-sans',
+  style: 'normal',
+  weight: '400 600 700',
+  display: "auto",
+  src: './static/fonts/SourceHanSansSC-VF.otf.woff2',
+})
 export const metadata: Metadata = {
   title: 'Next Blog',
   description: 'A blog build by Cloud0310 and NickId2018',
@@ -24,7 +39,14 @@ export default function RootLayout({
 }) {
   return (
     <html className={`${materialSymbols.variable}`}>
-    <body>{children}</body>
+    <body>
+    <div className="fixed w-16 flex justify-between">
+      <div className="py-2 px-1 flex items-center gap-2.5">
+        <span className="font-symbol w-7.5 h-7.5"> rocket </span>
+      </div>
+    </div>
+    {children}
+    </body>
     </html>
   )
 }
