02cba451b3ca9b8a4f0e4ffd46d2a85fd36f22e1
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2023-09-07 00:54:49 +0800

    TurboPack for dev

--- src/app/blog/[...slug]/layout.tsx
+++ src/app/blog/[...slug]/layout.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import * as React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative top-[60px] flex w-full flex-col items-center justify-center font-sans">
-      <div className="w-1/3"></div>
-      <div className="w-1/3">{children}</div>
-      <div className="w-1/3 flex-col items-start justify-start"></div>
+    <div className="relative top-[60px] flex h-full w-full justify-center font-sans">
+      <div className="flex-1"></div>
+      <div className="flex-1 lg:min-w-[700px]">{children}</div>
+      <div className="w-full flex-1">
+        <div className="sticky left-20 top-20 mx-6 my-10 h-[calc(100vh-20rem)] w-[200px] ">
+          <div className="my-1.5 rounded border font-sans font-bold text-neutral-300">
+            On this page
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
