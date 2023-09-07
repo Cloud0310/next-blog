a79cca5346fe323844d8c3b969cb8ec1f0d326cb
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2023-09-07 20:51:01 +0800

    bug fixes

--- src/app/blog/[...slug]/layout.tsx
+++ src/app/blog/[...slug]/layout.tsx
@@ -1,8 +1,19 @@
-import { ReactNode } from "react";
-
-function getCurrentReadingPart() {}
+"use client";
+import { ReactNode, useEffect } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const buttons = document.getElementsByClassName("copy-content");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", () => {
+        const button = buttons[i] as HTMLButtonElement;
+        navigator.clipboard.writeText(button.value).then((r) => {
+          button.innerText = "inventory";
+          setTimeout(() => (button.innerText = "content_paste"), 1000);
+        });
+      });
+    }
+  }, []);
   return (
     <div className="relative top-[60px] flex h-full w-full justify-center font-sans">
       <div className="flex-1 bg-neutral-100 "></div>
