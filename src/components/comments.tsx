37a7feaa3c169c99dd8d60f9729668fdda8024a7
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2023-09-29 00:47:08 +0800

    ？

---
+++ src/components/comments.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { DisqusJS } from "disqusjs/dist/react/disqusjs.es2022.es";
+import "disqusjs/dist/styles/disqusjs.css";
+
+export default function Comments() {
+  return (
+    <>
+      <DisqusJS shortname="" siteName="" identifier="" url="" api="" apikey="" admin="" adminLabel="" />
+    </>
+  );
+}
